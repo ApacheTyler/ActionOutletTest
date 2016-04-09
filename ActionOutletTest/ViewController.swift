43fca589a8bb1968444c4c010aa06bcba9879c6c
Tyler Freeman <tyler.freeman@modea.com>
2016-04-09 12:13:04 -0400

    Added button to set text to label and reset button to set back to original text

--- ActionOutletTest/ViewController.swift
+++ ActionOutletTest/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var defaultText : String = "Set me!"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func SetAsTextButton(sender: UIButton) {
+        self.TextDisplayLabel.text = self.LabelInputText.text
+    }
 
+    @IBAction func ResetButton(sender: UIButton) {
+        self.TextDisplayLabel.text = defaultText
+    }
+    
+    @IBOutlet weak var LabelInputText: UITextView!
+    @IBOutlet weak var TextDisplayLabel: UILabel!
+    
 }
 
